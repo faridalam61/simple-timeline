764d7da67e496b6e987a3cc3b5b749af8c1c6cd2
faridalam61 <faridalam62@gmail.com>
2023-08-13 12:44:07 +0600

    EntryCard compnent added

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,16 @@
 import './App.css'
-
+import EntryCard from './Components/EntryCard'
 function App() {
   
   return (
-    <>
+   
      <div className="timeline">
-  <div className="entry">
-    <div className="title">
-      <h3>2014 - Present</h3>
-      <p>Title, Company</p>
-    </div>
-    <div className="body">
-      <p>Voluptatibus veniam ea reprehenderit atque reiciendis non laborum adipisci ipsa pariatur omnis.</p>
-       <p>Voluptatibus veniam ea reprehenderit atque reiciendis non laborum adipisci ipsa pariatur omnis.</p>
-    </div>
-  </div>
-
-
-  <div className="entry">
-    <div className="title">
-      <h3>2010 - Present</h3>
-      <p>Title, Company</p>
-    </div>
-    <div className="body">
-      <p>Voluptatibus veniam ea reprehenderit atque reiciendis non laborum adipisci ipsa pariatur omnis.</p>
-    </div>
-  </div>
-  <div className="entry">
-    <div className="title">
-      <h3>2010 - Present</h3>
-      <p>Title, Company</p>
-    </div>
-    <div className="body">
-      <p>Voluptatibus veniam ea reprehenderit atque reiciendis non laborum adipisci ipsa pariatur omnis.</p>
-    </div>
-  </div>
-
-
-
-  <div className="entry">
-    <div className="title">
-      <h3>2009 - 2010</h3>
-      <p>Title, Company</p>
-    </div>
-    <div className="body">
-      <p>Voluptatibus veniam ea reprehenderit atque reiciendis non laborum adipisci ipsa pariatur omnis.</p>
-    </div>
-  </div>
-
-</div>
-    </>
+      <EntryCard/>
+      <EntryCard/>
+      <EntryCard/>
+      <EntryCard/>
+     </div>
+    
   )
 }
 
---
+++ src/Components/EntryCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function EntryCard() {
+  return (
+    <div className="entry">
+    <div className="title">
+      <h3>2009 - 2010</h3>
+      <p>Title, Company</p>
+    </div>
+    <div className="body">
+      <p>Voluptatibus veniam ea reprehenderit atque reiciendis non laborum adipisci ipsa pariatur omnis.</p>
+    </div>
+  </div>
+  )
+}
+
+export default EntryCard
