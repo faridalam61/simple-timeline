7526c11a555397dc217af6fd6ff410d294df4b17
faridalam61 <faridalam62@gmail.com>
2023-08-13 12:30:43 +0600

    Simple timeline app created

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+     <div className="timeline">
+  <div className="entry">
+    <div className="title">
+      <h3>2014 - Present</h3>
+      <p>Title, Company</p>
+    </div>
+    <div className="body">
+      <p>Voluptatibus veniam ea reprehenderit atque reiciendis non laborum adipisci ipsa pariatur omnis.</p>
+       <p>Voluptatibus veniam ea reprehenderit atque reiciendis non laborum adipisci ipsa pariatur omnis.</p>
+    </div>
+  </div>
+
+
+  <div className="entry">
+    <div className="title">
+      <h3>2010 - Present</h3>
+      <p>Title, Company</p>
+    </div>
+    <div className="body">
+      <p>Voluptatibus veniam ea reprehenderit atque reiciendis non laborum adipisci ipsa pariatur omnis.</p>
+    </div>
+  </div>
+  <div className="entry">
+    <div className="title">
+      <h3>2010 - Present</h3>
+      <p>Title, Company</p>
+    </div>
+    <div className="body">
+      <p>Voluptatibus veniam ea reprehenderit atque reiciendis non laborum adipisci ipsa pariatur omnis.</p>
+    </div>
+  </div>
+
+
+
+  <div className="entry">
+    <div className="title">
+      <h3>2009 - 2010</h3>
+      <p>Title, Company</p>
+    </div>
+    <div className="body">
+      <p>Voluptatibus veniam ea reprehenderit atque reiciendis non laborum adipisci ipsa pariatur omnis.</p>
+    </div>
+  </div>
+
+</div>
+    </>
+  )
+}
+
+export default App
